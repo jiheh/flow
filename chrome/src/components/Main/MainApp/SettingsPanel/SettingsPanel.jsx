55e93cb0498004027c7af97aafad7fa556fa4c38
Clement Mihailescu <clementmihailescu@gmail.com>
2016-11-21 23:53:21 -0500

    Design for Toggle Video / Photo Button is complete

--- chrome/src/components/Main/MainApp/SettingsPanel/SettingsPanel.jsx
+++ chrome/src/components/Main/MainApp/SettingsPanel/SettingsPanel.jsx
@@ -1,15 +1,25 @@
 import React, { PropTypes } from 'react';
-
 import { propTypes as settingsPropTypes } from '../../../../reducers/settings';
+import './SettingsPanel.scss';
+import photoLogo from './photo-camera.png';
+import videoLogo from './video-camera.png';
+
+console.log("HELLO!!!!!!!!!!!!!!!!")
+console.log()
 
 const SettingsPanel = ({
   toggleClock,
   toggleBackgroundMode,
   settings,
+  test
 }) => (
   <div className="settings-panel">
     <button onClick={toggleClock}>Toggle Clock</button>
-    <button onClick={toggleBackgroundMode}>Toggle Background</button>
+    <img
+      src={settings.showVideo ? photoLogo : videoLogo}
+      className="background-mode"
+      onClick={toggleBackgroundMode}
+    />
   </div>
 );
 

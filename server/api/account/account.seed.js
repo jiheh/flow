8a5ea0bce7f8aec2a18deb5831f95606acdd1839
Hussein Farah <husseinfarah93@gmail.com>
2016-11-22 11:20:57 -0500

    account promise resolved

--- server/api/account/account.seed.js
+++ server/api/account/account.seed.js
@@ -1,62 +1,43 @@
 const Account = require('../account/account.model')
 const Organization = require('../organization/organization.model')
 
-const createAccounts = (n) =>{
-  let arrToReturn = [];  
-  Account.create({
+const createAccounts = (n) => { 
+  return Account.create({
     type:'basic',
     maxStudents:10,
     cost:100
   })
-  .then(() =>{
-    Account.create({
+  .then(() => {
+    return Account.create({
       type:'medium',
       maxStudents:20, 
       cost:200
     })  
   })
-  .then(() =>{
-    Account.create({
+  .then(() => {
+    return Account.create({
       type:'pro',
       maxStudents:30, 
       cost:300
     })  
   })
   .then(() =>{
-    for(let i=0;i<n;i++){
+    const orgPromiseArr = [];
+    for(let i=1; i<=n; i++) {
       let organizationGlobal;
-      let randomNum = Math.floor(Math.random() * (n-1))
-      const organizationPromise = Organization.findById(i+1)
-      .then(organization =>{
+      let randomNum = Math.floor(Math.random() * 3) + 1;
+      const orgPromise = Organization.findById(i)
+      .then(organization => {
         organizationGlobal = organization
         return Account.findById(randomNum)
-      }).then(account =>{
+      }).then(account => {
+        console.log(organizationGlobal.id,account.id)
         return organizationGlobal.addAccount(account)
-      })
-      arrToReturn.push(organizationPromise)   
+      });
+      orgPromiseArr.push(orgPromise);
     }
-  }).then(() =>{
-    console.log(arrToReturn.length)
-    return Promise.all(arrToReturn)
+    return Promise.all(orgPromiseArr);
   })
-  
-  
-  
-  
-  
-  
-  // .then(() =>{
-  //   for(let i=0;i<n;i++){
-  //     let randomNum = Math.floor(Math.random() * 3) + 1 
-  //     let promiseToResolve = Organization.findById(i+1)
-  //     .then(organization =>{
-  //       Account.findById(randomNum)
-  //       .then(account =>{
-  //         return organization.addAccount(account)
-  //       })
-  //     })
-  //   }
-  // })
 }
 
 module.exports = createAccounts;

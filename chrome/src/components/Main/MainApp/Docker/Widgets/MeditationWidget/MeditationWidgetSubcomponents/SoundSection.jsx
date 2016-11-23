3f5b1abcf82a498412fd210ef55ea4433c6f19a4
Franklyn Zhu <zfranklyn@gmail.com>
2016-11-23 00:05:46 -0500

    meditations section has been set up, announcements UI has been redesigned and reimplemented

---
+++ chrome/src/components/Main/MainApp/Docker/Widgets/MeditationWidget/MeditationWidgetSubcomponents/SoundSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// import Line from '../WidgetComponents/Line.jsx';
+import '../../Widgets.scss';
+
+const SoundSection = ({  }) => (
+  <div className="sounds-section">
+    <h4>SOUNDS</h4>
+
+    <div className="grid">
+      <div className="grid-item">
+        <h4 className="grid-item-title">
+          Breathing Space
+        </h4>
+        <p className="grid-item-description">
+          3 min
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default SoundSection ;
--- chrome/src/components/Main/MainApp/Docker/Widgets/ReflectionWidget/ReflectionWidget.jsx
+++ chrome/src/components/Main/MainApp/Docker/Widgets/ReflectionWidget/ReflectionWidget.jsx
@@ -6,15 +6,13 @@ import '../Widgets.scss';
 const ReflectionWidget = ({  }) => (
   <div className="reflection-widget widget">
 
-    <h4 className="widget-title">
-      Reflection
-    </h4>
+    <div className="widget-nav">
+      <h4 className="widget-title">
+        REFLECTION
+      </h4>
+    </div>
 
     <Line />
-
-    <div className="widget-contents">
-
-    </div>
 
   </div>
 );

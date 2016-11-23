ecc7265ea50d2d3ae263cf834d15d82be5bee2ea
Jiheh Ritterling <jritterling@gmail.com>
2016-11-23 12:16:28 -0500

    wireframes created

---
+++ browser/components/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div className="home">
+    <div className="banner text-center text-inverted">
+      <h1>flow</h1>
+      <h1><small>This is the home component</small></h1>
+    </div>
+  </div>
+);

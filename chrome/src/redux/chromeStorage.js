656c9b53f4729215b390be98bb1c87446b64fd99
Anthony Ardito <anthony.ardito@gmail.com>
2016-11-25 22:42:46 -0500

    chrome storage sync can take array instead of just string - simplified loadFromStorage accordingly

--- chrome/src/redux/chromeStorage.js
+++ chrome/src/redux/chromeStorage.js
@@ -24,18 +24,12 @@ const chromeStorageMiddleware = (keys) => {
 
 // used for initial loading of values of keys specified
 // in keys array from chrome storage to redux store
-export const loadFromStorage = (keys /*, resolve */) => {
+export const loadFromStorage = (keys) => {
   if (!keys || !keys.length) { return Promise.resolve({}); }
 
-  return Promise.map(keys, (key) => {
-    return new Promise((resolve) => {
-      chrome.storage.sync.get(key, resolve);
-    });
-  })
-    .then((loaded) => {
-      // combine all loaded values into one object
-      return loaded.reduce((agg, obj) => _.merge(obj, agg), {});
-    });
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(keys, resolve);
+  });
 };
 
 export default chromeStorageMiddleware;

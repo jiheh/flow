e8962cdc183e5aa3ae8ab5ded1ad06fcfec3cf3f
Anthony Ardito <anthony.ardito@gmail.com>
2016-11-26 10:23:12 -0500

    Fixed video url

--- chrome/src/components/Main/Background/BackgroundVideo/BackgroundVideo.jsx
+++ chrome/src/components/Main/Background/BackgroundVideo/BackgroundVideo.jsx
@@ -5,7 +5,7 @@ const BackgroundVideo = () => (
   <div className="backround-video-wrapper">
     <video
       className="background-video"
-      src={`http://localhost:8080/backgroundVideos/${backgroundVideo}`}
+      src={'http://localhost:8080/api/videos/random'}
       autoPlay
       loop
     />

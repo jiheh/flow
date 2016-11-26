6988046317f60f37d3549f6001b4d88b1ff20bc2
Anthony Ardito <anthony.ardito@gmail.com>
2016-11-26 14:04:53 -0500

    Channel search autosuggest implemented

--- browser/components/Dashboard/Sidebar/Sidebar.jsx
+++ browser/components/Dashboard/Sidebar/Sidebar.jsx
@@ -2,21 +2,37 @@ import React, { PropTypes } from 'react';
 /* import { Link } from 'react-router';*/
 import './Sidebar.scss';
 import ChannelListContainer from './ChannelList/ChannelListContainer.js';
+import Autosuggest from 'react-autosuggest';
 
 const Sidebar = ({
-  handleSearchInput,
+  /* handleSearchInput,*/
+  handleSearchChange,
   searchInput,
+  onSuggestionsFetchRequested,
+  onSuggestionsClearRequested,
+  getSuggestionValue,
+  renderSuggestion,
+  suggestions,
+  shouldRenderSuggestions,
 }) => (
   <div className="sidebar">
     <div className="sidebar-nav">
       <div className="pt-input-group .modifier sidebar-nav-search">
         <span className="pt-icon pt-icon-search search-icon" />
-        <input
-          className="pt-input"
-          type="search"
-          placeholder="Search channels..."
-          id="channel_searchbox"
-          onInput={handleSearchInput}
+        <Autosuggest
+          inputProps={{
+            className: 'pt-input',
+            type: 'search',
+            placeholder: 'Search channels...',
+            id: 'channel_searchbox',
+            onChange: handleSearchChange,
+            value: searchInput,
+          }}
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
         />
       </div>
     </div>
@@ -27,8 +43,15 @@ const Sidebar = ({
 );
 
 Sidebar.propTypes = {
-  handleSearchInput: PropTypes.func.isRequired,
+  /* handleSearchInput: PropTypes.func.isRequired,*/
+  handleSearchChange: PropTypes.func.isRequired,
   searchInput: PropTypes.string.isRequired,
+  onSuggestionsFetchRequested: PropTypes.func.isRequired,
+  onSuggestionsClearRequested: PropTypes.func.isRequired,
+  getSuggestionValue: PropTypes.func.isRequired,
+  renderSuggestion: PropTypes.func.isRequired,
+  suggestions: PropTypes.array.isRequired,
+  shouldRenderSuggestions: PropTypes.func.isRequired,
 };
 
 export default Sidebar;
--- browser/components/Dashboard/Sidebar/SidebarComponent.jsx
+++ browser/components/Dashboard/Sidebar/SidebarComponent.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import Sidebar from './Sidebar.jsx';
 
@@ -7,21 +7,78 @@ class SidebarComponent extends Component {
     super(props);
     this.state = {
       searchInput: '',
+      suggestions: [],
     };
   }
 
-  handleSearchInput = (evt) => {
-    this.setState({ searchInput: evt.target.value });
+  onSuggestionsFetchRequested = ({ value }) => {
+    console.log('fetch suggestions');
+    console.log(value);
+    console.log('new suggestions: ', this.getSuggestions(value));
+    this.setState({
+      suggestions: this.getSuggestions(value),
+    });
   }
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+    });
+  }
+
+  getSuggestionValue = (suggestion) => {
+    return suggestion.name;
+  }
+
+  getSuggestions = (searchInput) => {
+    const { allChannels } = this.props;
+    const inputValue = searchInput.trim().toLowerCase();
+
+    if (inputValue.length > 0) {
+      return allChannels.filter((channel) => {
+        return channel.name.toLowerCase().includes(inputValue) ||
+              channel.description.toLowerCase().includes(inputValue);
+      });
+    } else {
+      return [];
+    }
+  }
+
+  shouldRenderSuggestions = (value) => {
+    return value.trim().length > 2;
+  }
+
+  handleSearchChange = (evt, { newValue }) => {
+    this.setState({
+      searchInput: newValue,
+    });
+  }
+
+  renderSuggestion = suggestion => (
+    <div>
+      {suggestion.name}
+    </div>
+  )
 
   render() {
     return (
       <Sidebar
-        handleSearchInput={this.handleSearchInput}
+        handleSearchChange={this.handleSearchChange}
         searchInput={this.state.searchInput}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={this.getSuggestionValue}
+        renderSuggestion={this.renderSuggestion}
+        allChannels={this.props.allChannels}
+        suggestions={this.state.suggestions}
+        shouldRenderSuggestions={this.shouldRenderSuggestions}
       />
     );
   }
 }
+
+SidebarComponent.propTypes = {
+  allChannels: PropTypes.array,
+};
 
 export default SidebarComponent;

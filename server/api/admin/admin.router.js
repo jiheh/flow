94fa2a9bc2b4f3d4f9eb7fc41fd2311457157161
Clement Mihailescu <clementmihailescu@gmail.com>
2016-11-26 14:58:01 -0500

    Fixed the capital I in the userInfo imports

--- server/api/admin/admin.router.js
+++ server/api/admin/admin.router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router(); // eslint-disable-line new-cap
 const Admin = require('./admin.model')
-const UserInfo = require('../userinfo/userinfo.model')
+const UserInfo = require('../userInfo/userInfo.model')
 
 //Create a new Organization
 router.post('/',(req,res,next) =>{

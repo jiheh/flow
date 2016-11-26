703ce16f4807a07524a064daacf4a08f438c1412
Anthony Ardito <anthony.ardito@gmail.com>
2016-11-26 05:52:45 -0500

    Added max age to background videos static route to prevent browser from refetching after every loop

--- server/app/statics.middleware.js
+++ server/app/statics.middleware.js
@@ -7,8 +7,29 @@ const router = express.Router();
 const path = require('path');
 
 const rootPath = path.join(__dirname, '..', '..');
-const publicPath = path.join(rootPath, 'public');
+const bundlePath = path.join(rootPath, 'public', 'bundle.js');
+const bundleMapPath = path.join(rootPath, 'public', 'bundle.js.map');
+const bundleMinPath = path.join(rootPath, 'public', 'bundle.min.js');
+const bundleMapMinPath = path.join(rootPath, 'public', 'bundle.min.js.map');
+const imagesPath = path.join(rootPath, 'public', 'backgroundImages');
+const videosPath = path.join(rootPath, 'public', 'backgroundVideos');
+const filesPath = path.join(rootPath, 'public', 'files');
+const quotesPath = path.join(rootPath, 'public', 'quotes');
+const faviconPath = path.join(rootPath, 'public', 'favicon.ico');
+const logoPath = path.join(rootPath, 'public', 'logo.png');
 
-router.use(express.static(publicPath));
+const cacheTime = '1d';
+router.use('/backgroundVideos', express.static(videosPath, { maxAge: cacheTime }));
+router.use('/backgroundImages', express.static(imagesPath));
+router.use('/files', express.static(filesPath));
+router.use('/quotes', express.static(quotesPath));
+router.use('/quotes', express.static(quotesPath));
+
+router.use('/bundle.js', express.static(bundlePath));
+router.use('/bundle.min.js', express.static(bundleMinPath));
+router.use('/bundle.js.map', express.static(bundleMapPath));
+router.use('/bundle.min.js.map', express.static(bundleMapMinPath));
+router.use('/favicon.ico', express.static(faviconPath));
+router.use('/logo.png', express.static(logoPath));
 
 module.exports = router;

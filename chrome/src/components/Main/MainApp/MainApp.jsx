5556036c17442ba7845c239cd2ba47f536c9c286
Clement Mihailescu <clementmihailescu@gmail.com>
2016-11-27 00:47:24 -0500

    Moved Logout button to Settings viewport and added CSS

--- chrome/src/components/Main/MainApp/MainApp.jsx
+++ chrome/src/components/Main/MainApp/MainApp.jsx
@@ -8,19 +8,16 @@ import './MainApp.scss';
 
 const MainApp = ({
   saveSettings,
-  logout,
 }) => (
   <div className="main-app">
     <DockerContainer />
     <ToggleBackgroundMode saveSettings={saveSettings} />
     <CenterPiece />
-    <button onClick={logout}>LOGOUT</button>
   </div>
 );
 
 MainApp.propTypes = {
   saveSettings: PropTypes.func.isRequired,
-  logout: PropTypes.func.isRequired,
 };
 
 export default MainApp;

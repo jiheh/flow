97be86715aebd13b5c5d783d0f6c42b5501bfe6b
Husseinfarah93 <husseinfarah93@gmail.com>
2016-11-28 15:10:26 -0500

    Update channel.router.js

--- server/api/channel/channel.router.js
+++ server/api/channel/channel.router.js
@@ -6,16 +6,17 @@ const User = require('../user/user.model');
 // Get all Channels for a specific admin
 router.get('/allChannels/', (req, res, next) => {
   if (!req.user) res.status(403).send();
-
-  Channel.findAll({
-    include: [{ all: true }],
-  })
-    .then((channels) => {
-      res.send(channels.filter(channel => {
-        return channel.admins.filter(admin => admin.id === req.user.id).length > 0;
-      }));
+  else{
+    Channel.findAll({
+      include: [{ all: true }],
     })
-    .catch(next);
+      .then((channels) => {
+        res.send(channels.filter(channel => {
+          return channel.admins.filter(admin => admin.id === req.user.id).length > 0;
+        }));
+      })
+      .catch(next);
+  }
 });
 
 

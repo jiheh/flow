534116f13f03062c87acfc1c953f2ab317d4ccd9
Hussein Farah <husseinfarah93@gmail.com>
2016-11-27 23:16:42 -0500

    correct description and numUsers in channel for dashboard

--- server/api/channel/channel.router.js
+++ server/api/channel/channel.router.js
@@ -1,14 +1,19 @@
 const router = require('express').Router(); // eslint-disable-line new-cap
 const Admin = require('../admin/admin.model')
 const Channel = require('./channel.model')
-
+const User = require('../user/user.model')
 // Get all Channels for a specific admin
-router.get('/allChannels',(req,res) =>{
-  Channel.findAll({})
+router.get('/allChannels',(req,res) =>{  
+  Channel.findAll({
+    include:[{
+      model:User
+    }]
+  })
   .then(channels =>{
     res.status(209).send(channels)
   })
 })
+
 
 // Get one Channel
 module.exports = router

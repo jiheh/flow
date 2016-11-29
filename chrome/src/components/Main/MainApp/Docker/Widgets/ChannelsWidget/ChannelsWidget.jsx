a6f2481789bc699fef91033426d3edf97ef6c995
aardito2 <anthony.ardito@gmail.com>
2016-11-28 20:48:13 -0500

    Update ChannelsWidget.jsx

--- chrome/src/components/Main/MainApp/Docker/Widgets/ChannelsWidget/ChannelsWidget.jsx
+++ chrome/src/components/Main/MainApp/Docker/Widgets/ChannelsWidget/ChannelsWidget.jsx
@@ -21,14 +21,11 @@ class ChannelsWidget extends Component {
         <Line />
 
         <div className="widget-contents">
-          { console.log("HARAMBE") }
-          { console.log(currentChannels) }
           { currentChannels && currentChannels.map((channel, index) => {
             return <div className="currentChannels" key={index}>{channel}</div>
           }) }
           { invites.length ? invites.map((invite, index) => {
             return <div className="channelInvitations" ref={index} key={index}>{invite.channelName} has invited you to its channel!<button onClick={() => {
-                console.log("EYYY HABIBI")
                 console.log(this.refs[index].className);
                 this.refs[index].className = "channelInvitationsDisplayNone";
                 console.log(this.refs[index].className);
@@ -41,51 +38,6 @@ class ChannelsWidget extends Component {
         </div>
       );
   }
-
-
-
 }
 
-
-// MeditationWidget.propTypes = {
-//   meditationWidget: propTypes,
-// };
-
 export default ChannelsWidget;
-
-
-//
-// const ChannelsWidget = ({
-//   invites,
-//   acceptAndDeleteInvite,
-//   currentChannels,
-//   showNewChannels,
-// }) => (
-//   <div className="channels-widget widget">
-//
-//     <div className="widget-nav">
-//       <h4 className="widget-title">
-//         CHANNELS
-//       </h4>
-//     </div>
-//
-//     <Line />
-//
-//     <div className="widget-contents">
-//       { console.log("HARAMBE") }
-//       { console.log(currentChannels) }
-//       { currentChannels && currentChannels.map((channel, index) => {
-//         return <div className="currentChannels" key={index}>{channel}</div>
-//       }) }
-//       { invites.length ? invites.map((invite, index) => {
-//         return <div className="channelInvitations" ref={index} key={index}>{invite.channelName} has invited you to its channel!<button onClick={() => {
-//             console.log("EYYY HABIBI")
-//             console.log(this.refs);
-//             showNewChannels(currentChannels, invite);
-//             acceptAndDeleteInvite(invite);
-//           }}>Accept</button></div>
-//         }) : <div className="channelInvitations">You have no channel invitations at this time.</div> }
-//       </div>
-//
-//     </div>
-//   );

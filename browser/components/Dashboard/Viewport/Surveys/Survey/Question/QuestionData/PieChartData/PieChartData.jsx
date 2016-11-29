1320e4cb40d78b8adf919c483dcec9c3eb3a2fa7
Jiheh Ritterling <jritterling@gmail.com>
2016-11-29 17:42:30 -0500

    pie chart has labels

--- browser/components/Dashboard/Viewport/Surveys/Survey/Question/QuestionData/PieChartData/PieChartData.jsx
+++ browser/components/Dashboard/Viewport/Surveys/Survey/Question/QuestionData/PieChartData/PieChartData.jsx
@@ -1,8 +1,7 @@
 import React, { PropTypes } from 'react';
-import { PieChart, Pie, Sector, Cell } from 'recharts';
+import { PieChart, Pie, Sector, Cell, Legend, ResponsiveContainer } from 'recharts';
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-
 
 const responsesToData = (responses) => {
   // responses:
@@ -23,21 +22,25 @@ const PieChartData = ({ responses }) => (
   <div>
     {responses.length === 0 ? 'No responses.' :
      (
-       <PieChart width={800} height={400}>
-        <Pie
-          data={responsesToData(responses)}
-          cx={120}
-          cy={200}
-          innerRadius={60}
-          outerRadius={80}
-          paddingAngle={5}
-        >
-         {responsesToData(responses).map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} key={index}/>)}
-        </Pie>
-       </PieChart>
+        <PieChart width={350} height={350} margin={{top: 0, right: 0, bottom: 0, left: 0}}>
+          <Pie
+            data={responsesToData(responses)}
+            cx='50%'
+            cy='70%'
+            innerRadius='60%'
+            outerRadius='70%'
+            paddingAngle={5}
+            label={(data) => `${data.name}`}
+          >
+
+           {responsesToData(responses).map((entry, index) => {
+            return(<Cell fill={COLORS[index % COLORS.length]} key={index} />)
+          })}
+          </Pie>
+        </PieChart>
      )
     }
-  </div>
+  </div>  
 );
 
 PieChartData.propTypes = {
--- browser/components/Dashboard/Viewport/Surveys/Survey/Question/QuestionData/QuestionData.jsx
+++ browser/components/Dashboard/Viewport/Surveys/Survey/Question/QuestionData/QuestionData.jsx
@@ -5,7 +5,7 @@ import TextData from './TextData/TextData.jsx';
 const QuestionData = ({ type, responses }) => (
   <div>
     {responses.length === 0 && 'No responses.'}
-    {responses.length > 0 && (type==='emoji' || type==='multiple_choice') && <PieChartData responses={responses} />}
+    {responses.length > 0 && (type==='emoji' || type==='multiple_choice' || type==='binary') && <PieChartData responses={responses} />}
     {responses.length > 0 && type==='text' && <TextData responses={responses} />}
   </div>
 );

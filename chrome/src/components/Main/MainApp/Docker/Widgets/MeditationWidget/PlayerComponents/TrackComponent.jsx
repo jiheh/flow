23b2178626ae4da44b8953a065b31b464084acb3
Franklyn Zhu <zfranklyn@gmail.com>
2016-11-30 00:06:49 -0500

    created prototype for meditation widget

--- chrome/src/components/Main/MainApp/Docker/Widgets/MeditationWidget/PlayerComponents/TrackComponent.jsx
+++ chrome/src/components/Main/MainApp/Docker/Widgets/MeditationWidget/PlayerComponents/TrackComponent.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-import '../PlayerComponents.scss';
+import './PlayerComponents.scss';
+import PlayButton from './PlayButtonComponent.jsx';
 
 const TrackComponent = ({ trackName, trackLength }) => (
   <div className="track">
-
+    <PlayButton />
+    <span className="track-name">{trackName}</span>
+    <span className="track-length">{trackLength}</span>
   </div>
 );
 
--- chrome/src/components/Main/MainApp/Docker/Widgets/MeditationWidget/example-files.js
+++ chrome/src/components/Main/MainApp/Docker/Widgets/MeditationWidget/example-files.js
@@ -1,12 +1,20 @@
 export const meditations = [
-  {title: "3 Minute Breathing Space", length: "5:02", url:""},
-  {title: "10 Minute Sitting Meditation", length: "9:55", url:""},
-  {title: "Mindful Breathing", length: "4:23", url:""},
-  {title: "Destress Meditation", length: "3:21", url:""},
+  {name: "1 Minute Breathing Space", length: "1:04", url:""},
+  {name: "3 Minute Breathing Space", length: "3:12", url:""},
+  {name: "5 Minute Breathing Space", length: "5:02", url:""},
+  {name: "Mindful Breathing", length: "4:23", url:""},
+  {name: "10 Minute Sitting Meditation", length: "9:55", url:""},
+  {name: "20 Minute Sitting Meditation", length: "9:55", url:""},
+  {name: "De-Stress Meditation", length: "3:21", url:""},
 ];
 export const sounds = [
-  {title: "Waves", length: "9:31", url:""},
-  {title: "Forest", length: "13:23", url:""},
-  {title: "Wind", length: "8:42", url:""},
+  {name: "Waves", length: "9:31", url:""},
+  {name: "Forest", length: "13:23", url:""},
+  {name: "Wind", length: "8:42", url:""},
+  {name: "Fire", length: "9:31", url:""},
+  {name: "Rain", length: "13:23", url:""},
+  {name: "Thunderstorm", length: "8:42", url:""},
+  {name: "Italian Coffee Shop", length: "8:42", url:""},
+  {name: "Autumn Nights", length: "8:42", url:""},
 ];
-export const visuals = [];
+

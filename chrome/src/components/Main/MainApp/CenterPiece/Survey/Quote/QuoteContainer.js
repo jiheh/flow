57cdcbba55bc9716e036b803ce33c3d8b36bb793
Franklyn Zhu <zfranklyn@gmail.com>
2016-12-01 17:04:20 -0500

    quote offline support implemented

--- chrome/src/components/Main/MainApp/CenterPiece/Survey/Quote/QuoteContainer.js
+++ chrome/src/components/Main/MainApp/CenterPiece/Survey/Quote/QuoteContainer.js
@@ -2,6 +2,9 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 import { setQuote } from '../../../../../../reducers/quote';
 import { setAuthor } from '../../../../../../reducers/author';
+import stockQuotes from './StockQuotes';
+import Chance from 'chance';
+const chance = new Chance();
 
 import QuoteComponent from './QuoteComponent.jsx';
 
@@ -17,14 +20,23 @@ const mapStateToProps = ({
 const mapDispatchToProps = () => dispatch => ({
   // gets random quote from server
   getQuote: () => {
-    // TODO: CHANGE TO PRODUCTION SERVER
-    axios.get(`http://localhost:8080/api/quotes/random`)
-      .then(res => {
-        dispatch(setQuote(res.data.quote));
-        dispatch(setAuthor(res.data.author));
-      })
-    // TODO: error handling
-      .catch(console.error);
+
+    if (navigator.onLine) {
+      // TODO: CHANGE TO PRODUCTION SERVER
+      axios.get(`http://localhost:8080/api/quotes/random`)
+        .then(res => {
+          dispatch(setQuote(res.data.quote));
+          dispatch(setAuthor(res.data.author));
+        })
+        // TODO: error handling
+        .catch(console.error);
+    } else {
+      const quote = chance.pickone(stockQuotes);
+      dispatch(setQuote(quote.quote));
+      dispatch(setAuthor(quote.author));
+    }
+
+
   },
 
   saveSettings: (settings) => {

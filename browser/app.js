7f4a5609bda6a047c99919079e5cc01e7850c6bd
Anthony Ardito <anthony.ardito@gmail.com>
2016-12-02 12:27:20 -0500

    console flow

--- browser/app.js
+++ browser/app.js
@@ -6,6 +6,14 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Routes from './Routes';
 
+console.log('       __ _               \n');
+console.log('      / _| |              \n');
+console.log('     | |_| | _____      __\n');
+console.log('     |  _| |/ _ \\ \\ /\\ / /\n');
+console.log('     | | | | (_) \\ V  V / \n');
+console.log('     |_| |_|\\___/ \\_/\\_/  \n');
+
+
 ReactDOM.render(
   <Provider store={store}>
     <Routes />

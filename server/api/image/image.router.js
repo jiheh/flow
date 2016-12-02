9cbdda039851ebee89d772d46aa3f716d8b8de6b
Anthony Ardito <anthony.ardito@gmail.com>
2016-12-02 09:56:50 -0500

    Image, video, quote now only change once a day

--- server/api/image/image.router.js
+++ server/api/image/image.router.js
@@ -6,12 +6,16 @@ const router = require('express').Router(); // eslint-disable-line new-cap
 
 router.get('/random', (req, res, next) => {
   const imageDir = path.join(__dirname, '..', '..', '..', 'public', 'backgroundImages');
+  const now = new Date();
+  const epochDays = Math.floor(now/8.64e7);
+
   fs.readdir(imageDir, (err, files) => {
     if (err) {
       next(err);
     } else {
       const numFiles = files.length;
-      const imageToSend = files[Math.floor(Math.random() * numFiles)];
+      const imageToSend = files[epochDays % numFiles];
+      // const imageToSend = files[Math.floor(Math.random() * numFiles)];
       const imagePath = path.join(imageDir, imageToSend);
       res.sendFile(imagePath);
     }

770167f359458a665093fcc47f030ab6f801cf10
Hussein Farah <husseinfarah93@gmail.com>
2016-12-05 02:03:01 -0500

    back to working demo data

--- browser/components/Dashboard/Viewport/Surveys/Survey/Question/QuestionData/LineChartData/LineChartData.jsx
+++ browser/components/Dashboard/Viewport/Surveys/Survey/Question/QuestionData/LineChartData/LineChartData.jsx
@@ -4,13 +4,13 @@ import { generateSurveyDates } from  './helpers'
 import './LineChartData.scss';
 
 const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page A', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: 'Page A', uv: 'a', pv: 2400, amt: 2400},
+      {name: 'Page A', uv: 'ab', pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 'abac', pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 'afbc', pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 'ahbc', pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 'aobc', pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 'aqqbc', pv: 4300, amt: 2100},
 ];
 
 
@@ -22,7 +22,7 @@ const LineChartData = ({ responses, survey }) => (
      (
         <LineChart width={600} height={300} data={data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
        <XAxis dataKey="pv"/>
-       <YAxis dataKey="name" type="category"/>
+       <YAxis type="category"/>
        <CartesianGrid strokeDasharray="3 3"/>
        <Tooltip/>
        <Legend />

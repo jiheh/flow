b1184b7e409bc341b160cb337d31cef25acf4ba0
Hussein Farah <husseinfarah93@gmail.com>
2016-12-04 23:13:53 -0500

    mid

--- browser/components/Dashboard/Viewport/Surveys/Survey/Question/QuestionData/LineChartData/LineChartData.jsx
+++ browser/components/Dashboard/Viewport/Surveys/Survey/Question/QuestionData/LineChartData/LineChartData.jsx
@@ -5,7 +5,7 @@ import './LineChartData.scss';
 
 const data = [
       {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page A', uv: 3000, pv: 1398, amt: 2210},
       {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
       {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
       {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
@@ -16,18 +16,17 @@ const data = [
 
 const LineChartData = ({ responses, survey }) => (
   <div>
-    {console.log('survey',survey)}
-    {console.log('responses',responses) }
+    {console.log('survey',survey.name,'surveyFrequency',survey.frequency.length)}
+    {console.log('responses',responses.length)}
     {responses.length === 0 ? 'No responses.' :
      (
         <LineChart width={600} height={300} data={data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-       <XAxis dataKey="name"/>
-       <YAxis/>
+       <XAxis dataKey="pv"/>
+       <YAxis dataKey="name" type="category"/>
        <CartesianGrid strokeDasharray="3 3"/>
        <Tooltip/>
        <Legend />
-       <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+       <Line type="monotone" dataKey="name" stroke="#8884d8" activeDot={{r: 8}}/>
       </LineChart>
      )
     }

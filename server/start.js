42c9ad1cf8da5d3bcc8d98013895d98e12157dec
Jiheh Ritterling <jritterling@gmail.com>
2016-12-05 14:21:20 -0500

    ready for heroku

--- server/start.js
+++ server/start.js
@@ -3,7 +3,7 @@
 const app = require('./app');
 const db = require('./db');
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 const server = app.listen(port, (err) => {
   if (err) throw err;
   console.log(`Server is listening to port ${port}!`);

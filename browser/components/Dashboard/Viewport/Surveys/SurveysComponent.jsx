bb942cce549dd15516ec69e289effb136da94e33
Jiheh Ritterling <jritterling@gmail.com>
2016-12-06 18:19:16 -0500

    surveys searchbar works

--- browser/components/Dashboard/Viewport/Surveys/SurveysComponent.jsx
+++ browser/components/Dashboard/Viewport/Surveys/SurveysComponent.jsx
@@ -12,8 +12,13 @@ class SurveysComponent extends Component {
 			showModal: false,
 			modalType: null, // "new_survey", "existing_survey"
 			currentSurveyID: null,
+			searchInput: ''
 		};
 	}
+
+	handleSearchInput = (evt) => {
+    this.setState({ searchInput: evt.target.value });
+  }
 
 	toggleNewSurveyForm = () => {
 		this.setState({showModal: !this.state.showModal, modalType: "new_survey" });
@@ -33,14 +38,25 @@ class SurveysComponent extends Component {
 
 		return (
 			<div>
+				<div className="pt-navbar survey-navbar">
+		      <div className="pt-navbar-group pt-align-left">
+		        <input className="pt-input" placeholder="Search Surveys..." type="text" onInput={this.handleSearchInput} />
+		      </div>
+
+		      <div className="pt-navbar-group pt-align-right">
+		        <button id='new-form-button' className='pt-button pt-intent-primary' onClick={this.toggleNewSurveyForm}>Create a New Survey</button>
+		      </div>
+		    </div>
+
 				<Surveys
-					toggleNewSurveyForm={this.toggleNewSurveyForm}
 					toggleExistingSurveyEditor={this.toggleExistingSurveyEditor}
 					currentChannel={currentChannel}
 					modalType={this.state.modalType}
 					showModal={this.state.showModal}
 					submitSurvey={submitSurvey}
-					currentSurveyID={this.state.currentSurveyID} />
+					currentSurveyID={this.state.currentSurveyID}
+					searchInput={this.state.searchInput}
+				/>
 			</div>
 		)
 	}

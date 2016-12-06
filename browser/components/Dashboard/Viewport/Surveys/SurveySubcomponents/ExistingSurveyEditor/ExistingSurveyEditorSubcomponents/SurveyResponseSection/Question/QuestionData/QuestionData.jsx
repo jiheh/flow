5a5eca8d90ec5a79b295b43e74aa72c9d6385a5a
Hussein Farah <husseinfarah93@gmail.com>
2016-12-06 18:47:49 -0500

    fixed black dot

--- browser/components/Dashboard/Viewport/Surveys/SurveySubcomponents/ExistingSurveyEditor/ExistingSurveyEditorSubcomponents/SurveyResponseSection/Question/QuestionData/QuestionData.jsx
+++ browser/components/Dashboard/Viewport/Surveys/SurveySubcomponents/ExistingSurveyEditor/ExistingSurveyEditorSubcomponents/SurveyResponseSection/Question/QuestionData/QuestionData.jsx
@@ -6,7 +6,6 @@ import LineChartData from './LineChartData/LineChartData.jsx'
 
 const QuestionData = ({ type, responses, survey }) => (
   <div>
-    {console.log('survey',survey)}
     {responses.length === 0 && 'No responses.'}
     {responses.length > 0 && (type==='emoji' || type==='multiple_choice' || type==='binary') && (!survey.frequency || survey.frequency.length === 0) && <PieChartData responses={responses} />}
     {responses.length > 0 && (type==='emoji' || type==='multiple_choice' || type==='binary') && (survey.frequency && survey.frequency.length > 0) && <LineChartData responses={responses} survey={survey} />}

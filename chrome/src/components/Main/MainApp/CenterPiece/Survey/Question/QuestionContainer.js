78fc4c27a241b309c79427a5cd055f1e307f7a26
Jiheh Ritterling <jritterling@gmail.com>
2016-12-05 19:18:33 -0500

    webapp deployment

--- chrome/src/components/Main/MainApp/CenterPiece/Survey/Question/QuestionContainer.js
+++
@@ -1,5 +0,0 @@
-import { connect } from 'react-redux';
-
-import QuestionComponent from  './QuestionComponent.jsx';
-
-export default connect()(QuestionComponent);
--- chrome/src/components/Main/MainApp/Docker/Widgets/WidgetComponents/Line.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './WidgetComponentStyles.scss';
-
-const Line = () => (
-  <div className="horizontal-line"></div>
-);
-
-
-export default Line;

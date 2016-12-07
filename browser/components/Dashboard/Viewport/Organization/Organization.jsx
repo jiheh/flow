e42ffe4e911ab6830ca1d366e0e03e835bda60bf
Jiheh Ritterling <jritterling@gmail.com>
2016-12-07 02:27:57 -0500

    instructions added

--- browser/components/Dashboard/Viewport/Organization/Organization.jsx
+++ browser/components/Dashboard/Viewport/Organization/Organization.jsx
@@ -5,6 +5,7 @@ const Organization = ({ organization }) => (
     <hr />
 			<h4>{`This channel belongs to the ${organization.name} Organization.`}</h4>
 			<p>Use the buttons above to create and track surveys, to create and view announcements, to invite members, and to add admins to your channel.</p>
+			<p>Please note, members and admins must be chrome extension users, and surveys cannot be created for a channel until there are users on that channel.</p>
   </div>
 );
 

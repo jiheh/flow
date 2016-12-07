a0a34f6c37818d3c0d10e3b8f8eb5b43a51d021e
aardito2 <anthony.ardito@gmail.com>
2016-12-07 00:27:01 -0500

    Update store.js

--- browser/store.js
+++ browser/store.js
@@ -5,8 +5,10 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
+const PROD = (process.env.NODE_ENV === 'production');
+
 const enhancer = compose(
-  applyMiddleware(thunk, createLogger()),
+  applyMiddleware(PROD ? thunk : ...[thunk, createLogger()]),
   persistState(['currentAdmin']),
 )
 

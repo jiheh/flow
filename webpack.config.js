43aa346812988cacb0ce060b1029b59cbed8ae5e
Jiheh Ritterling <jritterling@gmail.com>
2016-12-09 12:43:04 -0500

    removed uglifyjsplugin

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
         NODE_ENV: JSON.stringify('production')
       }
     }),
-    new webpack.optimize.UglifyJsPlugin(),
+    // new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.DedupePlugin(),
     new webpack.EnvironmentPlugin(["NODE_ENV"]),
   ] : [
